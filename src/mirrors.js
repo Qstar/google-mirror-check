1268d9869ff007dcbda23d10faafe81d29433fae
jyboy <hitwangjunyang@163.com>
2016-05-06 00:00:13 +0800

    镜像地址小幅增删

--- src/mirrors.js
+++ src/mirrors.js
@@ -3,6 +3,7 @@ module.exports = [
     'http://ggss.cf/',
     'http://soguge.com/',
     'http://www.wen.lu/',
+    'http://wen.lu/',
     'http://www.zzq2.com/',
     'https://g2.wen.lu/',
     'https://gg.kfd.me/',
@@ -11,6 +12,5 @@ module.exports = [
     'https://www.google.com.hk/webhp?hl=zh-CN',
     'https://www.ggss.cf/?gws_rd=ssl',
     'https://ma.liu.ba/',
-    'http://libingjie.com/',
-    'http://www.gglink.pw/'
+    'http://libingjie.com/'
 ];
